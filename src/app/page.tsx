da116393d7807994fa70353641cb9288675e5b66
unknown <zekipossible@gmail.com>
2025-02-28 17:39:18 -0800

    fifth-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import Header from "@/component/Header/Header";
 import Mission from "@/component/Mission/Mission";
 import Vision from "@/component/Vision/Vision";
@@ -12,7 +12,9 @@ import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <div className="bg-black min-h-screen">
-      <Header />
+      <section>
+        <Header />
+      </section>
       <main className="container mx-auto p-4">
         <section>
           <HeroPage />
@@ -23,7 +25,7 @@ export default function Home() {
         <section className="mt-10">
           <History />
         </section>
-        <section className="mt-10"></section>
+        <section className="mt-10">
           <Company />
         </section>
         <section className="mt-10">
