64f55fd73ad30b6a43edfa2957fe83d90e4148d5
unknown <zekipossible@gmail.com>
2025-03-02 22:46:00 +0100

    size-change

--- src/component/Vision/Vision.tsx
+++ src/component/Vision/Vision.tsx
@@ -17,7 +17,7 @@ const Vision: React.FC = () => {
         </div>
         <div className="md:w-1/2 w-full transform transition-transform duration-500 hover:translate-x-2 hover:scale-x-105">
           <div className="w-[500px] p-10 ">
-            <p className="text-white text-center md:text-left w-[600px] p-6">
+            <p className="text-white text-center md:text-left w-[400px] p-6">
               Our vision is to build successful and long-lasting relationships
               based on quality, integrity, commitment, and responsibility. We
               aim to provide the right product, at the right time, and for the
