cf23dafeff6764de9bcc5898714295c3b18eef2c
unknown <zekipossible@gmail.com>
2025-03-02 21:51:41 +0100

    Name-Improved

--- src/component/Team/Team.tsx
+++ src/component/Team/Team.tsx
@@ -4,19 +4,19 @@ import Image from "next/image";
 const teamMembers = [
   {
     id: 1,
-    name: "John Doe",
+    name: "Prince (Dr) Andrew Lucky Eseigbe IGBERAESE, D. Engr, M, SC, FCP, MIEEE, FIMC, CMC",
     role: "CEO",
     image: "/images/m0.jpg",
     content:
-      "John Doe is the CEO of Lucktang Int'l Limited. With over 20 years of experience in the marine industry, he leads the company with a vision for innovation and excellence.",
+      "Prince (Dr) Andrew Lucky Eseigbe IGBERAESE is the CEO of Lucktang Int'l Limited. With over 20 years of experience in the marine industry, he leads the company with a vision for innovation and excellence.",
   },
   {
     id: 2,
-    name: "Jane Smith",
+    name: "Njideka Nwalor  BSC, BSN, Hon. D.N",
     role: "COO",
     image: "/images/me4.jpg",
     content:
-      "Jane Smith is the COO of Lucktang Int'l Limited. She oversees the company's operations, ensuring efficiency and effectiveness in all processes.",
+      "Njideka Nwalor is the COO of Lucktang Int'l Limited. She oversees the company's operations, ensuring efficiency and effectiveness in all processes.",
   },
   {
     id: 3,
@@ -31,7 +31,9 @@ const teamMembers = [
 const Team: React.FC = () => {
   return (
     <div className="container mx-auto p-8">
-      <h2 className="text-3xl font-bold text-blue-500 text-center mb-8">Meet Our Team</h2>
+      <h2 className="text-3xl font-bold text-blue-500 text-center mb-8">
+        Meet Our Team
+      </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {teamMembers.map((member) => (
           <div key={member.id} className="bg-black shadow-md p-6 rounded-2xl">
